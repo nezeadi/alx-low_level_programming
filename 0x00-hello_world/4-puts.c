6f63d8f9478c43a9bd978535785a05402ed5b41a
nezeadi <nezeadi@gmail.com>
2023-02-16 21:32:58 +0000

    adding 4-puts v1.1 : My firs alx C program

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,9 @@
 #include <stdio.h>
 
+/*entry point - main function*/
 int main(void)
 {
 	/*displaying the text*/
-	puts("Programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
