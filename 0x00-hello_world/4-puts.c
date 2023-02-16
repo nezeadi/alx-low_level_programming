ed1bc6045e5bc53418b324549beb00b7ab1d17a5
nezeadi <nezeadi@gmail.com>
2023-02-16 23:18:46 +0000

    adding 4-puts v1.4 : Betty please let my first program run

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,15 +1,14 @@
 #include <stdio.h>
 /**
  * main - Entry point
- * 
- * Descripton - a program to print text using puts function
+ * Descripton: a program to print text using puts function
  *
- * return: Always 0 (Success)
+ * Return: Always 0 (Success)
  */
 int main(void)
 {
 	/*puts functions displays text*/
-	puts("\'Programming is like building a multilingual puzzle");
-	/*end of program*/
+	puts("\"Programming is like building a multilingual puzzle");
+	/*Description: end of program*/
 	return (0);
 }
