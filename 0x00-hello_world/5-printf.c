82d5d342e30c0d3c8f8144b87cb73f610bf7f20a
nezeadi <nezeadi@gmail.com>
2023-02-16 23:43:05 +0000

    adding 5-printf.c : Betting and I get each other now

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+*main - testing printf function
+*Return: 0
+*/
+
+int main(void)
+{
+	/*printf function to display text*/
+	printf("with proper grammar, but the outcome is a piece of art,");
+	return (0);
+}
