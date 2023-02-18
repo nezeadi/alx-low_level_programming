29cc3f03d20b4ca1508ac49d0a73c2da5548bba0
nezeadi <nezeadi@gmail.com>
2023-02-18 16:26:16 +0000

    adding 5-print_numbers.c

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+/**
+ * main - a program that prints 0-9
+ * Return: Always 0
+ */
+
+int main(void)
+{
+	int i = 0;
+
+	while (i <= 9)
+	{
+		printf("%d", i);
+		i++;
+	}
+        printf("\n");
+	return (0);
+}
