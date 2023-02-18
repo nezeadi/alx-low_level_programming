dae5a87969c921e82cbed0c09da2435027a16951
nezeadi <nezeadi@gmail.com>
2023-02-18 00:37:50 +0000

    adding 101-quote.c

---
+++ 0x00-hello_world/101-quote.c
@@ -0,0 +1,13 @@
+#include <stdio.h>
+
+/**
+*main - program to print to std error
+*Return: always 1
+*/
+
+int main(void)
+{
+	write(2, "and that piece of art is useful\" = Dora korpar, 2015-10-19\n", 59);
+	return (1);
+}
+
