518abdfffe7ef2e5d66a43ef000925571f2595a8
nezeadi <nezeadi@gmail.com>
2023-02-18 11:35:45 +0000

    adding 2-print_alphabet.c

---
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+*main - a program to print eh alphabet in lowercase
+*Return: Always 0
+*/
+
+int main(void)
+{
+	char letter;
+
+	while (letter < 'z')
+	{
+		putchar(letter\n);
+		putchar('\n');
+		letter++
+	}
+	return (0);
+}
