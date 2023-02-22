8c158476282e27444d7edf1ca27416c76b499cb1
nezeadi <nezeadi@gmail.com>
2023-02-22 15:57:51 +0000

    commit 0.1

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,34 @@
+#include "main.h"
+
+/**
+ * main - a program to print the alphabet in lower case
+ * Return: 0
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}#include "main.h"
+
+/**
+ * main - a program to print the alphabet in lower case
+ * Return: 0
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}#include "main.h"
+
+/**
+ * main - a program to print the alphabet in lower case
+ * Return: 0
+ */
+
+int main(void)
+{
+	print_alphabet();
+	return (0);
+}
