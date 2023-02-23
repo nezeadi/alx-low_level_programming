cbaf4cc422865c589cf61398e3147c71cdc40cef
nezeadi <nezeadi@gmail.com>
2023-02-23 07:12:05 +0000

    add 11-print_to_98.c v1.1

--- 0x02-functions_nested_loops/11-print_to_98.c
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -12,7 +12,7 @@ void print_to_98(int n)
 	{
 		while (n >= 98)
 		{
-			_putchar(n);
+			_putchar(n + '0');
 			_putchar(',');
 			n--;
 		}
@@ -21,7 +21,7 @@ void print_to_98(int n)
 	{
 		while (n <= 98)
 		{
-			_putchar(n);
+			_putchar(n + '0');
 			_putchar(',');
 			n++;
 		}
