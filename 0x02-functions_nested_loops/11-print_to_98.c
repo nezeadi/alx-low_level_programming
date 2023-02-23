3398a380987820faf7b9d3835baea7b7a1f7a9bd
nezeadi <nezeadi@gmail.com>
2023-02-23 07:09:49 +0000

    add 11-print_to_98.c

---
+++ 0x02-functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,33 @@
+#include "main.h"
+
+/**
+ * print_to_98 - a number which prints to 98 for any side of it
+ *@n: function argument which is the count starting point
+ * Return: nothing
+ */
+
+void print_to_98(int n)
+{
+	if (n > 98)
+	{
+		while (n >= 98)
+		{
+			_putchar(n);
+			_putchar(',');
+			n--;
+		}
+	}
+	else if (n < 98)
+	{
+		while (n <= 98)
+		{
+			_putchar(n);
+			_putchar(',');
+			n++;
+		}
+	}
+	else
+	{
+		_putchar(n);
+	}
+}
