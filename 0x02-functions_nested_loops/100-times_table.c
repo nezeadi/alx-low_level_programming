363373d949e617d0e4b0d32d00f57269e7d3a0c9
nezeadi <nezeadi@gmail.com>
2023-02-23 18:04:46 +0000

    bug fix in 100-print_times_table.c 1.7

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -30,15 +30,13 @@ void print_times_table(int n)
 				else if (product < 10)
 				{
 					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
+					_putchar('\t');
 					_putchar(product + '0');
 				}
 				else
 				{
 					_putchar(',');
-					_putchar(' ');
-					_putchar(' ');
+					_putchar('\t');
 					_putchar(product / 10 + '0');
 					_putchar(product % 10 + '0');
 				}
