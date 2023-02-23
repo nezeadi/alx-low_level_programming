60a118a262d98cdf084ab701f1a7e4bb3819b4d2
nezeadi <nezeadi@gmail.com>
2023-02-23 18:45:47 +0000

    bug fix in 100-print_times_table.c 2.1

--- 0x02-functions_nested_loops/100-times_table.c
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -35,14 +35,21 @@ void print_times_table(int n)
 					_putchar(' ');
 					_putchar(product + '0');
 				}
+				else if (product < 100)
+				{
+					_putchar(',');
+					_putchar(' ');
+					_putchar((product / 100) + '0');
+					_putchar(((product / 10) % 10) + '0');
+					_putchar((product % 10) + '0');
+				}
 				else
 				{
 					_putchar(',');
 					_putchar(' ');
 					_putchar(' ');
-					_putchar(' ');
-					_putchar(product / 10 + '0');
-					_putchar(product % 10 + '0');
+					_putchar((product / 10) + '0');
+					_putchar((product % 10) + '0');
 				}
 			}
 			_putchar('\n');
