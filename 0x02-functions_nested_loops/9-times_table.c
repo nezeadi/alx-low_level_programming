ed441a41ab508ee0d9e7b71aa76cc3b2991fd942
nezeadi <nezeadi@gmail.com>
2023-02-23 04:50:48 +0000

    add 9-times_table.c v1.4

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -19,15 +19,16 @@ void times_table(void)
 			}
 			else if (product < 10)
 			{
-				_putchar(product);
 				_putchar(',');
 				_putchar(' ');
+				_putchar(product + '0');
 			}
 			else
-			{
-				_putchar(product / 10 + '0);
+			{	
+				_putchar(',');
+				_putchar(' ');
+				_putchar(product / 10 + '0');
 				_putchar(product % 10 + '0');
-				_putchar('\n');
 			}
 		}
 		_putchar('\n');
