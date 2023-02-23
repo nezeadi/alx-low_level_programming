14be99fa840db145d2a689caf3c6467fc99e494a
nezeadi <nezeadi@gmail.com>
2023-02-23 04:47:04 +0000

    add 9-times_table.c v1.3

--- 0x02-functions_nested_loops/9-times_table.c
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -13,10 +13,22 @@ void times_table(void)
 		for (j = 0; j <= 9; j++)
 		{
 			int product = i * j;
-
-			_putchar(product);
-			_putchar(',');
-			_putchar(' ');
+			if (product == 0)
+			{
+				_putchar('0');
+			}
+			else if (product < 10)
+			{
+				_putchar(product);
+				_putchar(',');
+				_putchar(' ');
+			}
+			else
+			{
+				_putchar(product / 10 + '0);
+				_putchar(product % 10 + '0');
+				_putchar('\n');
+			}
 		}
 		_putchar('\n');
 	}
