c3538b06d36455fdea057645f26dbc2b9fb53f89
nezeadi <nezeadi@gmail.com>
2023-02-23 04:39:56 +0000

    add 9-times_table.c

---
+++ 0x02-functions_nested_loops/9-times_table.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * times_table - a function to print the 9 times table
+ */
+
+void times_table(void);
+{
+	int i, j;
+
+	for (i = 0; i <= 9; i++)
+	{
+		for (j = 0; j <= 9; j++)
+		{
+			int product = i * j;
+
+			_putchar(product);
+			_putchar(',');
+			_putchar(' ');
+		}
+		_putchar('\n');
+	}
+}
+
+
