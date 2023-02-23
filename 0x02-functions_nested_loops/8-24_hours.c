b62a2377f64c03bcdde00b9230edf89a35a0db1a
nezeadi <nezeadi@gmail.com>
2023-02-23 04:12:51 +0000

    add 8-24_hours.c

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+
+/**
+ * jack_bauer - a function that prints every minute
+ */
+
+void jack_bauer(void)
+{
+	int hr, min;
+
+	for (hr = 0; hr < 24; hr++)
+	{
+		for (min = 0; min < 60; min++)
+		{
+			_putchar (hr / 10 + '0');
+			_putchar (hr % 10 + '0');
+			_putchar (':');
+			_putchar (min / 10 + '0');
+			_putchar (min % 10 + '0');
+			_putchar ('\n');
+		}
+	}
+}
