0f017700356ab3daab451474e4d0eab8c37a2f65
nezeadi <nezeadi@gmail.com>
2023-02-23 03:14:32 +0000

    add 5-sign.c v1.1

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -11,22 +11,16 @@ int print_sign(int n)
 	if (n > 0)
 	{
 		_putchar('+');
-		_putchar(', ');
-		_putchar('\n');
 		return (1);
 	}
 	else if (n == 0)
 	{
-		_putchar(0);
-		_putchar(', ');
-		_putchar('\n');
+		_putchar('0');
 		return (0);
 	}
 	else
 	{
 		_putchar('+');
-		_putchar(', ');
-		_putchar('\n');
 		return (-1);
 	}
 }
