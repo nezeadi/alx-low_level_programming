81f5b525d66992db24545b52e7d1cda05f0649cc
nezeadi <nezeadi@gmail.com>
2023-02-24 20:28:42 +0000

    add 0-3-print_numbers.c

---
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * print_numbers - a function to print numbers 0-9
+ * Return: nothing
+ */
+
+void print_numbers(void)
+{
+	int i;
+
+	for (i = 0; i <= 9; i++)
+	{
+		_putchar(i + '0');
+	}
+	_putchar('\n');
+}
