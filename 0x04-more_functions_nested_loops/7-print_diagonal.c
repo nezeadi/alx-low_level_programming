43c6e03385af504b70bb61b98b7695fac4325ff4
nezeadi <nezeadi@gmail.com>
2023-02-24 22:29:10 +0000

    add 0-7-print_diagonal.c v1.1

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -8,7 +8,7 @@
 
 void print_diagonal(int n)
 {
-	int i;
+	int i, j;
 
 	if (n <= 0)
 	{
@@ -18,9 +18,10 @@ void print_diagonal(int n)
 	{
 		for (i = 1; i <= n; i++)
 		{
+			for (j = 1; j < i; j++)
+				_putchar(' ');
 			_putchar('\\');
 			_putchar('\n');
 		}
 	}
 }
-
