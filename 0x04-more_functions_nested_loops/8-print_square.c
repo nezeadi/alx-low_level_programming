71576088cb37f04b5fc145e8fcb1d1fe6dbba8ff
nezeadi <nezeadi@gmail.com>
2023-02-24 23:44:12 +0000

    add 0-8-print_square.c v1.1

---
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -0,0 +1,21 @@
+#include "main.h"
+
+/**
+ * print_square - a function to print a square using '#'
+ * @size: function argument specifing dimension of square
+ * Return: nothing
+ */
+
+void print_square(int size)
+{
+	int i, j;
+
+	for (i = 1; i <= size; i++)
+	{
+		for (j = 1; j <= size; j++)
+		{
+			_putchar('#');
+		}
+		_putchar('\n');
+	}
+}
