c2fc7a8000c027e33160939439b9dcae9613dc6e
nezeadi <nezeadi@gmail.com>
2023-02-24 21:44:09 +0000

    add 0-5-more_numbers.c v1.2

--- 0x04-more_functions_nested_loops/5-more_numbers.c
+++ 0x04-more_functions_nested_loops/5-more_numbers.c
@@ -13,7 +13,11 @@ void more_numbers(void)
 	{
 		for (j = 0; j <= 14; j++)
 		{
-			_putchar(i + '0');
+			if (j > 9)
+			{
+				_putchar((j / 10) + '0');
+			}
+			_putchar((j % 10) + '0');
 		}
 		_putchar('\n');
 	}
