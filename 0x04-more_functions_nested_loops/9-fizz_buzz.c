8c0e2e45febad3b38af470132dbd2e9f9cbbb023
nezeadi <nezeadi@gmail.com>
2023-02-25 00:31:08 +0000

    add 0-9-fizz_buzz v1.2

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -14,22 +14,19 @@ int main(void)
 
 	for (i = 1; i <= 100; i++)
 	{
-		if ((i % 3 == 0) && (i % 5 == 0))
+		if ((i % 3 == 0) || (i % 5 == 0))
 		{
-			printf("FizzBuzz ");
-		}
-		else if (i % 5 == 0)
-		{
-			printf("Buzz ");
-		}
-		else if (i % 3 == 0)
-		{
-			printf("Fizz ");
+			if (i % 3 == 0)
+				printf("Fizz");
+			if (i % 5 == 0)
+				printf("Buzz");
 		}
 		else
 		{
-			printf("%d ", i);
+			printf("%d", i);
 		}
+		if (i != 100)
+			printf(" ");
 	}
 	printf("\n");
 	return (0);
