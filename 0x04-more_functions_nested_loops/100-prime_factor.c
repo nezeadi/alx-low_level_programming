5408bba4287b98fff2ac31f715f3a390b19c4793
nezeadi <nezeadi@gmail.com>
2023-02-25 15:45:10 +0000

    add 100-prime_factor.c

---
+++ 0x04-more_functions_nested_loops/100-prime_factor.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+
+/**
+ * main - function to find largest prime number
+ *
+ * Return: 0
+ */
+
+int main(void)
+{
+	unsigned int a = 2;
+	unsigned long n = 612852475143;
+
+	while (a <= n/2)
+	{
+		if (n % a == 0)
+		{
+			n = n / a;
+		}
+		else
+		{
+			a++;
+		}
+	}
+	printf("%lu\n", n);
+	return (0);
+}
