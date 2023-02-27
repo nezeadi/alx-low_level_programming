e764b6ca967124e7f9fee1f3ce351d1e9c0826a0
nezeadi <nezeadi@gmail.com>
2023-02-27 02:09:51 +0000

    update 101-print_number.c - update 1.1

--- 0x04-more_functions_nested_loops/101-print_number.c
+++ 0x04-more_functions_nested_loops/101-print_number.c
@@ -8,37 +8,16 @@
 
 void print_number(int n)
 {
-	/*First we determine the lenth of integer*/
-	int integer_length = 0;
-	int i;
-	int m = n;
 
-	while (n != 0)
+	if (n < 0)
 	{
-		n = n / 10;
-		integer_length++;
+		_putchar('-');
+		n = -n;
 	}
 
-
-	/**
-	 * initiate a loop with a size of interger lenth
-	 * within the loop use putchar to display value of integer
-	 * modulo 10 to the power of current value of loop counter
-	 */
-
-	if (integer_length <= 1)
+	if (n / 10)
 	{
-		_putchar('0' + m);
-	} else if (integer_length == 2)
-	{
-		_putchar('0' + (m / 10));
-		_putchar('0' + (m % 10));
-	} else
-	{
-		_putchar('0' + (m / (10 ^ (integer_length - 1))));
-		for (i = integer_length - 2; i >= 0; i--)
-		{
-			_putchar('0' + (m / (10 ^ i) % 10));
-		}
+		print_number(n / 10);
 	}
+		_putchar(n % 10 + '0');
 }
