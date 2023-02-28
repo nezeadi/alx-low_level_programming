ce072217d50c30bd5dc66367c274a5dfcf010e01
nezeadi <nezeadi@gmail.com>
2023-02-28 18:22:58 +0000

    add 2-strlen.c v1.2 betty compliant

--- 0x05-pointers_arrays_strings/2-strlen.c
+++ 0x05-pointers_arrays_strings/2-strlen.c
@@ -2,7 +2,7 @@
 
 /**
  * _strlen - a function that returns the lenght of a string
- * @s - pointer argument referencing the
+ * @s: pointer argument referencing the
  * address of string whose length is returned
  * Return: integer length of the string parameter
  */
