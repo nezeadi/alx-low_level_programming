9c7b11c86c7073d855eef4ee8107c40042066d46
nezeadi <nezeadi@gmail.com>
2023-03-01 17:58:51 +0000

    compilation bug fix 2.0

--- 0x05-pointers_arrays_strings/100-atoi.c
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -1,5 +1,5 @@
 #include "main.h"
-#include "2-strlen.c"
+
 
 /**
  * _atoi - function to convert string to int
@@ -9,20 +9,22 @@
 
 int _atoi(char *s)
 {
-	int i, num;
 	int sign = 1;
+	unsigned int num;
 
-	for (i = 0; i < _strlen(s); i++)
+	while (*s != '\0' && (*s < '0' || *s > '9'))
 	{
-		if (s[i] >= '0' && s[i] <= '9')
-		{
-			num = (num * 10) + (s[i] - '0');
-		}
-
-		if (s[i] == '-')
+		if (*s == '-')
 		{
 			sign *= -1;
 		}
+		s++;
+	}
+
+	while (*s >= '0' && *s <= '9')
+	{
+		num = (num * 10) + (*s - '0');
+		s++;
 	}
 	return (sign * num);
 }
