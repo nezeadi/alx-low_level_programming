b871dbcca2221681eba8fdd4c4322e323c124a14
nezeadi <nezeadi@gmail.com>
2023-03-01 04:07:10 +0000

    add 7-puts_half.c v1.1

--- 0x05-pointers_arrays_strings/7-puts_half.c
+++ 0x05-pointers_arrays_strings/7-puts_half.c
@@ -3,7 +3,7 @@
 
 /**
  * puts_half - a function that prints half of a string
- * @str - string pointer to be printed
+ * @str: string pointer to be printed
  */
 
 void puts_half(char *str)
@@ -12,14 +12,14 @@ void puts_half(char *str)
 
 	if (_strlen(str) % 2 == 0)
 	{
-		for (i = _strlen(str) / 2; i <= _strlen(str); i++)
+		for (i = _strlen(str) / 2; i < _strlen(str); i++)
 		{
 			_putchar(str[i]);
 		}
 	}
 	else
 	{
-		for (i = (_strlen(str) - 1) / 2; i <= _strlen(str); i++)
+		for (i = (_strlen(str) - 1) / 2; i < _strlen(str); i++)
 		{
 			_putchar(str[i]);
 		}
