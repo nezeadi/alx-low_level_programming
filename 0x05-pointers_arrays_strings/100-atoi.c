95571cd653c6460738d36514c99bb19b36a33331
nezeadi <nezeadi@gmail.com>
2023-03-01 17:30:59 +0000

    add 100-atoi.c

---
+++ 0x05-pointers_arrays_strings/100-atoi.c
@@ -0,0 +1,29 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * _atoi - function to convert string to int
+ * @s: function argument. pointer to string to be converted
+ * Return: interger value in string
+ */
+
+int _atoi(char *s)
+{
+	int i, num;
+	int sign = 1
+
+	for (i = 0, i < _strlen(s); i++)
+	{
+		if (s[i] >= '0' && s[i] <= '9')
+		{
+			num = (num * 10) + (s[i] - '0');
+		}
+
+		if (s[i] == '-')
+		{
+			sign *= -1;
+		}
+	}
+	return (sign * num);
+}
+
