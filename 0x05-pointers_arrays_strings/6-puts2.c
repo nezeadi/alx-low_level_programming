55c1cd570ef8afaf16504bf805cb991f18be0ff8
nezeadi <nezeadi@gmail.com>
2023-03-01 03:46:04 +0000

    add 6-puts2.c

---
+++ 0x05-pointers_arrays_strings/6-puts2.c
@@ -0,0 +1,19 @@
+#include "main.h"
+#include "2-strlen.c"
+
+/**
+ * puts2 - function to print every other char in a string
+ * @str: string to be printed in required format
+ */
+
+void puts2(char *str)
+{
+	int i;
+
+	for (i = 0; i < _strlen(str) -1; i++)
+	{
+		if (i % 2 == 0)
+			_putchar(str[i]);
+	}
+	_putchar('\n');
+}
