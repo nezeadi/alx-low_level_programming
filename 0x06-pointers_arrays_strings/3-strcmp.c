eb279fe96f2aaeb682ba406bb75c1aa509359b03
nezeadi <nezeadi@gmail.com>
2023-03-02 21:29:08 +0000

    add 3-strcmp.c bug fixed

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -9,13 +9,13 @@
 
 int _strcmp(char *s1, char *s2)
 {
-	if (s1 > s2 || s1 == s2)
+	int i = 0;
+
+	while (s1[i] - s2[i] == 0 && s1[i] != '\0')
 	{
-		return (s1 - s2);
+		i++;
 	}
-	else
-	{
-		return (s2 -s1);
-	}
+	return (s1[i] - s2[i]);
+
 }
 
