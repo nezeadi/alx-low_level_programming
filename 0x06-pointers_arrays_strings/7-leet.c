e4afce0771f50e5331a1488778505ce932cfa449
nezeadi <nezeadi@gmail.com>
2023-03-03 07:06:29 +0000

    add 7-leet.c

--- 0x06-pointers_arrays_strings/7-leet.c
+++ 0x06-pointers_arrays_strings/7-leet.c
@@ -1,3 +1,27 @@
 #include "main.h"
 
 /**
+ * *leet - function that encodes a string into 1337
+ * @s: string argument to be encoded
+ * Return: char array of new encoded string
+ */
+
+char *leet(char *s)
+{
+	char arry[20] = "a4A4e3E3o0O0t7T7l1L1"
+	int i, j;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; arr[j] != 0; j++)
+		{
+			if (s[j] == arr[j])
+			{
+				s[i] = arr[j + 1];
+				break;
+			}
+		}
+	}
+	return (s);
+}
+
