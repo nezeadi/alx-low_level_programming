8706c2c7f28a0a3d5af26c48e1f9ff0378a8cb01
nezeadi <nezeadi@gmail.com>
2023-03-08 21:02:29 +0000

    3-strspn.c bug fix 1.2

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -12,16 +12,18 @@ unsigned int _strspn(char *s, char *accept)
 	int i, j;
 	unsigned int k = 0;
 
-	for (i = 0; accept[i] != '\0'; i++)
+	for (i = 0; s[i] != '\0'; i++)
 	{
 		for (j = 0; s[j] != '\0'; j++)
 		{
-			if (accept[i] == s[j])
+			if (s[i] == accept[j])
 			{
 				k++;
 				break;
 			}
 		}
+		if (s[j] == '\0')
+			return (k);
 	}
 	return (k);
 }
