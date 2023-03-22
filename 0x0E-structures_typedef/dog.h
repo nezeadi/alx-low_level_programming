57293cc9964bbdd60374318762c2e7f5edc5a074
nezeadi <nezeadi@gmail.com>
2023-03-22 14:48:43 +0000

    update all files

--- 0x0E-structures_typedef/dog.h
+++ 0x0E-structures_typedef/dog.h
@@ -16,5 +16,21 @@ typedef struct dog
 	char *owner;
 } dog;
 
+/**
+ * struct dog_t - a new type of dog
+ */
+
+typedef struct dog_t
+{
+	char *name;
+	float age;
+	char *owner;
+}dog_t;
+
 int _putchar(char c);
+void init_dog(struct dog *d, char *name, float age, char *owner);
+void print_dog(struct dog *d);
+dog_t *new_dog(char *name, float age, char *owner);
+void free_dog(dog_t *d);
+
 #endif
