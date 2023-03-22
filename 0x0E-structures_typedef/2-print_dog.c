234fdb8e57eedca2cfea06068c127d3179ef3dd9
nezeadi <nezeadi@gmail.com>
2023-03-22 16:58:58 +0000

    update 2-print_dog.c and 2-main.c

---
+++ 0x0E-structures_typedef/2-print_dog.c
@@ -0,0 +1,42 @@
+#include "dog.h"
+#include <stdio.h>
+
+/**
+ * print_dog - function to print details of a dog
+ * @d: struct a dog
+ * Return: nothing
+ */
+
+void print_dog(struct dog *d)
+{
+	if (d)
+	{
+		if (d->name == NULL)
+		{
+			printf("Name: (nil)\n");
+		}
+		else
+		{
+			printf("Name: %s\n", d->name);
+		}
+
+		if (d->age == 0.0)
+		{
+			printf("Age: (nil)\n");
+		}
+		else
+		{
+			printf("Age: %.6f\n", d->age);
+		}
+
+		if (d->owner == NULL)
+		{
+			printf("Owner: (nil)\n");
+		}
+		else
+		{
+			printf("Owner: %s\n", d->owner);
+		}
+	}
+}
+
