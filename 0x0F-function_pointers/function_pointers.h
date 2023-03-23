dc018541c7affb2a1e6eb5942e9d2929b69a39ce
nezeadi <nezeadi@gmail.com>
2023-03-23 15:56:07 +0000

    add function_pointers.h

--- 0x0F-function_pointers/function_pointers.h
+++ 0x0F-function_pointers/function_pointers.h
@@ -1,6 +1,6 @@
 #include <stddef.h>
-#ifndef FUNCTION_POINTERS_H
-#define FUNCTION_POINTERS_H
+#ifndef FUNCTION_POINTER_H
+#define FUNCTION_POINTER_H
 
 int _putchar(char c);
 void print_name(char *name, void (*f)(char *));
