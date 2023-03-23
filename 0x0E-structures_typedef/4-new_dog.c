989a8546f7f5c9f6e013d4f2aa5bcc89f1da383d
nezeadi <nezeadi@gmail.com>
2023-03-23 09:58:00 +0000

    update 4-main.c and 4-new_dog.c

---
+++ 0x0E-structures_typedef/4-new_dog.c
@@ -0,0 +1,57 @@
+#include "dog.h"
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * new_dog - function to create a new dog
+ *
+ * @name: dog's name
+ * @age: dog's age
+ * @owner: dog's owner
+ *
+ * Return: pointer to struct dog or NULL
+ */
+
+dog_t *new_dog(char *name, float age, char *owner)
+{
+	dog_t *aja;
+	int length;
+	char *n;
+
+	if (name == 0 || owner == 0)
+		return (NULL);
+
+	aja = malloc(sizeof(dog_t));
+	if (aja == NULL)
+		return (NULL);
+
+	for (length = 1, n = name; *n; length++)
+		n++;
+	aja->name = malloc(length);
+	if (aja->name == 0)
+	{
+		free(aja);
+		return (NULL);
+	}
+
+	for (length = 1, n = owner; *n; length++)
+		n++;
+	aja->owner = malloc(length);
+	if (aja->owner == 0)
+	{
+		free(aja->name);
+		free(aja);
+		return (NULL);
+	}
+
+	for (length = 0; *name != 0; length++, name++)
+		aja->name[length] = *name;
+	aja->name[length] = 0;
+
+	for (length = 0; *owner != 0; length++, owner++)
+		aja->name[length] = *name;
+	aja->name[length] = 0;
+	aja->age = age;
+
+	return (aja);
+}
