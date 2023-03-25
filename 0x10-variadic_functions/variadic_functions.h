b4e5659ca63a6072a353886f5738a2c03232db10
nezeadi <nezeadi@gmail.com>
2023-03-25 11:52:48 +0000

    update variadic_functions.h

--- 0x10-variadic_functions/variadic_functions.h
+++ 0x10-variadic_functions/variadic_functions.h
@@ -9,4 +9,16 @@ void print_all(const char * const format, ...);
 
 int _putchar(char c);
 
+/**
+ * struct func_index - index for selecting functions to use for printing
+ * - for task 4 - print_all
+ *
+ *   @f: function to use
+ *   @d_type: data type to be printed
+ */
+typedef struct func_index
+{
+	void (*f)(va_list);
+	char d_type;
+} func_index;
 #endif
