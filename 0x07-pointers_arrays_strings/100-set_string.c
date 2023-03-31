429a6f38dea29a0a4ab69ac83f42b6c8baecf75e
nezeadi <nezeadi@gmail.com>
2023-03-31 13:12:38 +0000

    add 100-set_string.c and update main.h

---
+++ 0x07-pointers_arrays_strings/100-set_string.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * set_string - function to set the value of a pointer
+ * @s: string to set
+ * @to: value to set string s to
+ * Return: nothing
+ */
+void set_string(char **s, char *to)
+{
+	*s = to;
+}
