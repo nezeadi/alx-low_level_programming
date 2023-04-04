382fd8b404b7f8f786fb6dceb478584f05a3753f
nezeadi <nezeadi@gmail.com>
2023-04-04 13:40:56 +0000

    bug fix on task 3

--- 0x13-more_singly_linked_lists/3-add_nodeint_end.c
+++ 0x13-more_singly_linked_lists/3-add_nodeint_end.c
@@ -13,6 +13,9 @@ listint_t *add_nodeint_end(listint_t **head, const int n)
 	listint_t *new_node = malloc(sizeof(listint_t));
 	listint_t *last = *head;
 
+	if (new_node == NULL)
+		return (NULL);
+
 	/*Insert data into the new node*/
 	new_node->n = n;
 
