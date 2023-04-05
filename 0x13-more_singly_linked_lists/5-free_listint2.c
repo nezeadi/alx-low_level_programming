a665113459bc726959eda7fb9af5f8739dffc332
nezeadi <nezeadi@gmail.com>
2023-04-05 00:05:09 +0000

    bug fix on task 5

--- 0x13-more_singly_linked_lists/5-free_listint2.c
+++ 0x13-more_singly_linked_lists/5-free_listint2.c
@@ -12,9 +12,6 @@ void free_listint2(listint_t **head)
 	if (head == NULL || *head == NULL)
 		return;
 
-	tmp = (*head)->next;
-	*head = tmp;
-
 	for (tmp = (*head)->next; *head != NULL; *head = tmp)
 	{
 		tmp = (*head)->next;
