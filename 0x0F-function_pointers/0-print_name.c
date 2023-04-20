afd33b961e07611904b35331296893dbdd4599d9
nezeadi <nezeadi@gmail.com>
2023-04-20 15:18:43 +0000

    task 0 of function pointers done

---
+++ 0x0F-function_pointers/0-print_name.c
@@ -0,0 +1,12 @@
+#include "function_pointers.h"
+
+/**
+ * print_name - function to print a name
+ * @name: name to be printed
+ * @f: function to be called back to print a name
+ * Return: nothing
+ */
+void print_name(char *name, void (*f)(char *))
+{
+	f(name);
+}
---
+++ 0x0F-function_pointers/README.md
@@ -0,0 +1,3 @@
+#LEARNING HOW TO USE FUNCTION POINTERS
+
+## Task 0 a function that prints a name
