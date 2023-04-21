660239b5f479b4a613ed67bdd88644c0f934ed6d
nezeadi <nezeadi@gmail.com>
2023-04-21 21:43:36 +0000

    fix bug and update code to betty v1.1

--- 0x10-variadic_functions/2-print_strings.c
+++ 0x10-variadic_functions/2-print_strings.c
@@ -14,15 +14,13 @@ void print_strings(const char *separator, const unsigned int n, ...)
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
+		str = va_arg(args, char *);
 		if (i > 0 && separator != NULL)
 			printf("%s", separator);
-
-		str = va_arg(args, char*);
 		if (str == NULL)
-		{
 			printf("(nil)");
-		}
-		printf("%s", str);
+		else
+			printf("%s", str);
 	}
 	va_end(args);
 	printf("\n");
