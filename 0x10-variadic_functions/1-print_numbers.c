0227f142be9b484f4dd90921a6a121cbdd07b53f
nezeadi <nezeadi@gmail.com>
2023-04-21 06:10:00 +0000

    bug fixes on task 1 and make it betty compliant

--- 0x10-variadic_functions/1-print_numbers.c
+++ 0x10-variadic_functions/1-print_numbers.c
@@ -10,13 +10,10 @@ void print_numbers(const char *separator, const unsigned int n, ...)
 	unsigned int i;
 	va_list args;
 
-	if (n == 0)
-		exit(98);
-
 	va_start(args, n);
 	for (i = 0; i < n; i++)
 	{
-		if(i > 0 && separator != NULL)
+		if (i > 0 && separator != NULL)
 			printf("%s", separator);
 		printf("%d", va_arg(args, int));
 	}
