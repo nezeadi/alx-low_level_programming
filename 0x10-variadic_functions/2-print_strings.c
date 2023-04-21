1698b15931afc85400ad3e2f319d0d908990d373
nezeadi <nezeadi@gmail.com>
2023-04-21 21:31:19 +0000

    task 2 done and README updated

---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,30 @@
+#include "variadic_functions.h"
+/**
+ * print_strings - function that prints all its parameters
+ * @separator: string to be printed between numbers
+ * @n: number of parameters in the list
+ * Return: nothing
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *str;
+	va_list args;
+
+	va_start(args, n);
+	for (i = 0; i < n; i++)
+	{
+		if (i > 0 && separator != NULL)
+			printf("%s", separator);
+		
+		str = va_arg(args, char*);
+		if (str == NULL)
+		{
+			printf("(nil)");
+			exit(98);
+		}
+		printf("%s", str);
+	}
+	va_end(args);
+	printf("\n");
+}
