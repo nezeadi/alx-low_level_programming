cf32e515ab3adcafffabbd1aef90b6b5abd8f5d4
nezeadi <nezeadi@gmail.com>
2023-04-29 16:31:47 +0000

    make betty compliant

--- 0x12-singly_linked_lists/1-list_len.c
+++ 0x12-singly_linked_lists/1-list_len.c
@@ -3,7 +3,7 @@
 /**
  * list_len - returns number of elements in a linked list
  * @h: pointer to the head of the list
- * @Return: number of elements in the list
+ * Return: number of elements in the list
  */
 size_t list_len(const list_t *h)
 {
