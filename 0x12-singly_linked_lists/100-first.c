d582b22c3d938e81fae895cf44c4a0484ab68a5b
nezeadi <nezeadi@gmail.com>
2023-04-30 03:31:39 +0000

    bug fix 1-2 and README update

--- 0x12-singly_linked_lists/100-first.c
+++ 0x12-singly_linked_lists/100-first.c
@@ -8,6 +8,6 @@ void beforeMain(void) __attribute__((constructor));
 
 void beforeMain(void)
 {
-	printf("you're beat! and yet, you must allow,\n"
+	printf("You're beat! and yet, you must allow,\n"
 			"I bore my house upon my back!\n");
 }
