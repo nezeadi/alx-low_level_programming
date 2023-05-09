686c22a4c61fe3d6a38e0eb83c633b06298b9d07
nezeadi <nezeadi@gmail.com>
2023-05-09 04:37:25 +0000

    task 1 done

---
+++ 0x15-file_io/1-create_file.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * create_file - function to create a file
+ * @filename: name of file to create
+ * @text_content: content to be written to the file
+ * Return: 1 on success and -1 on failure
+ */
+
+int create_file(const char *filename, char *text_content)
+{
+	int fd_open, fd_write;
+
+	if (filename == NULL)
+		return (-1);
+
+	/*create file*/
+	fd_open = open(filename, O_RDWR | O_CREAT | O_TRUNC, 0600);
+
+	/*write content to file*/
+	fd_write = write(fd_open, text_content, sizeof(text_content));
+
+	if (fd_open == -1 || fd_write == -1)
+		return (-1);
+
+	close(fd_open);
+	return (1);
+}
